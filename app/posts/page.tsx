b6258cee11fa18a8acf1d7dc4a7219b394e299ea
matisosagit <matiassosa1314@gmail.com>
2026-04-09 11:17:17 -0300

    feat: I added a debouncing mechanism to avoid hitting the API with unnecessary requests

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,22 +1,37 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Post } from "@/types/post";
 import { InputSearch } from "@/components/inputSearch";
 import useSWR from "swr";
 import { toast } from "sonner";
 
+function useDebounce<T>(value: T, delay: number): T {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+        setDebouncedValue(value);
+        }, delay);
+
+        return () => clearTimeout(handler);
+    }, [value, delay]);
+
+    return debouncedValue;
+}
+
 export default function Posts() {  
     const [userId, setUserId] = useState<number | null>(null);
+    const debouncedSearch = useDebounce(userId, 500);
     const fetcher = (url: string) => fetch(url).then(res => res.json());
 
     const { data: allPosts = [], error: errorAll, isLoading: loadingAll } = useSWR("/api/get", fetcher,{
-        loadingTimeout: 100,
+        loadingTimeout: 2000,
         onLoadingSlow: () => {
             toast.warning("bad connction, waiting for response...")
         }
     });
-    const { data: userPosts = [], error: errorUser, isLoading: loadingUser } = useSWR(userId ? `/api/posts?userId=${userId}` : null, fetcher, {
-        loadingTimeout: 1000,
+    const { data: userPosts = [], error: errorUser, isLoading: loadingUser } = useSWR(debouncedSearch ? `/api/getbyid?userId=${debouncedSearch}` : null, fetcher, {
+        loadingTimeout: 2000,
         onLoadingSlow: () => {
             toast.warning("bad connction, waiting for response...")
         }
@@ -35,7 +50,10 @@ export default function Posts() {
     return (
         <main className="w-full h-full flex flex-col gap-4 p-4">
             <div className="flex justify-center">
-                <InputSearch placeholder="Search posts by userId..." onSearch={(query) => setUserId(parseInt(query))} />
+                <InputSearch 
+                    placeholder="Search posts by userId..." 
+                    onValueChange={(value : string) => setUserId(parseInt(value))}
+                />
             </div>
             <div className="flex flex-wrap gap-4 items-center justify-center">
                 {posts.map((post) => (
--- components/inputSearch.tsx
+++ components/inputSearch.tsx
@@ -2,11 +2,11 @@
 
 interface InputSearchProps {
     placeholder?: string;
-    onSearch: (query: string) => void;
+    onValueChange: (value: string) => void;
 }
 
 
-export const InputSearch = ({ placeholder, onSearch }: InputSearchProps) => {
+export const InputSearch = ({ placeholder, onValueChange }: InputSearchProps) => {
     return (
         <form className="flex justify-center items-center w-full h-full">   
             <label htmlFor="search" className="block mb-2.5 text-sm font-medium text-heading sr-only ">Search</label>
@@ -14,10 +14,10 @@ export const InputSearch = ({ placeholder, onSearch }: InputSearchProps) => {
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                     <svg className="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/></svg>
                 </div>
-                <input type="search" id="search" className=" flex w-auto p-3 ps-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" placeholder={placeholder} required />
-                <button type="button" className=" relative  text-white bg-brand hover:bg-brand-strong box-border border border-white focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded text-xs px-3 py-1.5 focus:outline-none" onClick={() => onSearch((document.getElementById("search") as HTMLInputElement).value)}>
+                <input type="search" id="search" className=" flex w-auto p-3 ps-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" placeholder={placeholder} onChange={(e) => onValueChange(e.target.value)} required />
+                {/* <button type="button" className=" relative  text-white bg-brand hover:bg-brand-strong box-border border border-white focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded text-xs px-3 py-1.5 focus:outline-none" onClick={() => onSearch((document.getElementById("search") as HTMLInputElement).value)}>
                     Search
-                </button>
+                </button> */}
             </div>
         </form>
     )
